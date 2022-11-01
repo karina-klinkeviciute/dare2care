77507c5ee858132090638912211e524c5541be6c
Karina <karina.klinkeviciute@gmail.com>
2022-11-01 17:27:54 +0200

    fixed bug src not scr

--- dare2care-quiz.js
+++ dare2care-quiz.js
@@ -14,7 +14,7 @@ for (let group in groups)
             // adding image
             let image = situations_group.situations[situation]["image"]
             let imageDiv = document.getElementById("image")
-            imageDiv.innerHTML = `<img scr="images/${image}" alt="">`
+            imageDiv.innerHTML = `<img src="images/${image}" alt="">`
             // imageDiv.style.display="inline-block"
 
             // adding description
